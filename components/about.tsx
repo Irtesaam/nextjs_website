cfb597a5a4c8ac8a3f8a833ecbda248e82a6462f
Irtesaam Atfi <atfimdirtesaam@gmail.com>
2024-09-22 16:23:09 +0530

    used &apos;

--- components/about.tsx
+++ components/about.tsx
@@ -44,9 +44,9 @@ export default function About() {
         developer.
         </p>
         <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
+        <span className="italic">When I&apos;m not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy <span className="font-medium">learning new things</span>. I am currently
-        learning about <span className="font-medium">history and philosophy</span>. I'm also learning how to play the guitar.
+        learning about <span className="font-medium">history and philosophy</span>. I&apos;m also learning how to play the guitar.
         </p>
     </motion.section>
   )
